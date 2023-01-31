ed499f967dedc3e45d60c35f5db66bcb26e8b5d6
Istvan <olivaistvan@gmail.com>
2023-01-31 00:48:21 -0300

    feat: render <Header /> in all pages

--- portfolio/src/Pages/Contato/Contato.js
+++ portfolio/src/Pages/Contato/Contato.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import Header from '../../Components/Header/Header';
+
 function Contato() {
     return (
         <div>
-            <h1>Hello !</h1>
+            <Header />
+            <h1>Contato !</h1>
         </div>
     );
 }
