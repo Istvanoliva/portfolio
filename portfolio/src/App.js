fdcd85baaad678a84b7c70c1b768086badda977f
Istvan <olivaistvan@gmail.com>
2023-01-30 23:43:32 -0300

    feat: (App) create routes

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Inicio from './Pages/Inicio/Inicio';
+import Projetos from './Pages/Projetos/Projetos';
+import Contato from './Pages/Contato/Contato';
+
 import './App.css';
 
 function App() {
     return (
-        <div>
-            <h1>Hello !</h1>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route exact path='/' element={ <Inicio /> } />
+                <Route exact path='/projetos' element={ <Projetos /> } />
+                <Route exact path='/contato' element={ <Contato /> } />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
