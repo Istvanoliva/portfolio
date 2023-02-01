581aaa30f48c206c9a45d85db21e65ed2be7ed7c
Istvan <olivaistvan@gmail.com>
2023-02-01 19:19:25 -0300

    feat:(Projects) create projects list

---
+++ portfolio/src/Projects/Projects.js
@@ -0,0 +1,49 @@
+const Projects = [
+    {
+        name: 'Trunfo',
+        img: 'img',
+        description: 'xxxxxxxxxxxxxxxxxxxxxxx',
+        url: 'url.******',
+        category: 'Front-end',
+        tolls: ['React', 'Javascript'],
+        git: 'KKKK',
+    },
+    {
+        name: 'Trybetunes',
+        img: 'img',
+        description: 'xxxxxxxxxxxxxxxxxxxxxxx',
+        url: 'url.******',
+        category: 'Front-end',
+        tolls: ['React', 'Javascript'],
+        git: 'KKKK',
+    },
+    {
+        name: 'Wallet',
+        img: 'img',
+        description: 'xxxxxxxxxxxxxxxxxxxxxxx',
+        url: 'url.******',
+        category: 'Front-end',
+        tolls: ['React', 'Javascript'],
+        git: 'KKKK',
+    },
+    {
+        name: 'Recepies App',
+        img: 'img',
+        description: 'xxxxxxxxxxxxxxxxxxxxxxx',
+        url: 'url.******',
+        category: 'Front-end',
+        tolls: ['React', 'Javascript'],
+        git: 'KKKK',
+    },
+    {
+        name: 'Star wars',
+        img: 'img',
+        description: 'xxxxxxxxxxxxxxxxxxxxxxx',
+        url: 'url.******',
+        category: 'Front-end',
+        tolls: ['React', 'Javascript'],
+        git: 'KKKK',
+    },
+];
+
+export default Projects;
