3fdab12f3de58144696dc6d376a62eaaf0bfb5b6
Istvan <olivaistvan@gmail.com>
2023-02-08 20:32:47 -0300

    feat: (Inicio) Implements new section

--- portfolio/src/Pages/Inicio/Inicio.js
+++ portfolio/src/Pages/Inicio/Inicio.js
@@ -1,12 +1,26 @@
 import React from 'react';
 
 import Header from '../../Components/Header/Header';
+import img from '../../images/Perfil-2023.jpg';
 
 function Inicio() {
     return (
         <div>
             <Header />
-            <h1>Início !</h1>
+            <section>
+                <img src={ img } />
+                <div>
+                    <h1>ISTVAN OLIVA<span>.</span></h1>
+                </div>
+                <div>
+                    <h2>Front-end Developer</h2>
+                </div>
+                <div>
+                    <p>Gosto de trabalhar com criação.</p>
+                    <p>Aprendendo a aprender.</p>
+                    <p>Em busca de novos desafios.</p>
+                </div>
+            </section>
         </div>
     );
 }
