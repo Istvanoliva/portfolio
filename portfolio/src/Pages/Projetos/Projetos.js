05c4413668e4a81008bee3aac518d12c89d65540
Istvan <olivaistvan@gmail.com>
2023-02-08 17:42:03 -0300

    refactor: (Projetos)

--- portfolio/src/Pages/Projetos/Projetos.js
+++ portfolio/src/Pages/Projetos/Projetos.js
@@ -1,54 +1,103 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Select from 'react-select';
 
 import Header from '../../Components/Header/Header';
 import ProjectCard from '../../Components/ProjectCard/ProjectCard';
-import Projects from '../../Projects/Projects';
+import { Projects, stacks, categories } from '../../Projects/Projects';
 
 function Projetos() {
+    const [ projects, setProjects ] = useState(Projects);
+    const [ searchName, setSearchName ] = useState('');
+    const [ category, setCategory ] = useState('Todas');
+    const [ options, setOptions ] = useState([]);
+
+    const optionsFilter = () => {
+        setProjects(Projects);
+        if (!options.length) return;
+        
+        const stacks = options.map((item) => item.value);
+        const arrays = [];
+
+        stacks.forEach((stack) => {
+            const filters = projects.filter((proj) => proj.tolls.some((proj) => proj.includes(stack)));
+            arrays.push(...filters);
+        });
+
+        const result = [...new Set(arrays.flat())];
+        setProjects(result);
+    };
+
+    const clear = () => {
+        setProjects(Projects);
+        setOptions([]);
+        setSearchName('');
+        setCategory('Todas');
+    };
+
     return (
         <div>
             <Header />
-            <h1>Projetos !</h1>
-            <form>
-                <label htmlFor="search">
+            <h1>Projetos</h1>    
+            <label htmlFor="search">
                 Buscar projetos: 
-                    <input
-                        type="text"
-                        id="search"
-                        name="search"
-                        placeholder="Nome, linguagem, categoria, etc..."
-                        // onChange={ handleChange }
-                        // value={ name }
-                    />
-                </label>
-                <label htmlFor="search-category">
-                Categoria: 
-                    <select
-                        name="category"
-                        id="category"
-                        // value={ category }
-                        // onChange={ handleChange }
-                    >   
-                        <option value='todas'>Todas</option>
-                        <option value='front-end'>Front-end</option>
-                        <option value='back-end'>Back-end</option>
-                    </select>
-                </label>
-            </form>
+                <input
+                    type="text"
+                    id="search"
+                    name="search"
+                    placeholder="Buscar por nome"
+                    onChange={ ({target}) => setSearchName(target.value) }
+                    value={ searchName }
+                />
+            </label>
+
+            <Select
+                placeholder={ category }
+                options={ categories }
+                name="category"
+                defaultValue={ category }
+                isClearable={ true }
+                isSearchable={ true }
+                isMulti={ false }
+                value={ category }
+                onChange={ (target) => setCategory(target.value) }
+            />
+
+            <Select
+                options={ stacks }
+                name="stacks"
+                isClearable={ true }
+                isSearchable={ true }
+                isMulti={ true }
+                value={ options }
+                onChange={ (target) => (setOptions(target), setProjects(Projects)) }
+            />
+
+            <button onClick={ () => optionsFilter() }>Filtrar</button>
+            <button onClick={ () => clear() }>Limpar</button>
+
             <div>
                 {
-                    Projects.map((proj) => (
-                        <ProjectCard
-                            key={ proj.name }
-                            name={ proj.name }
-                            img={ proj.img }
-                            description={ proj.description }
-                            url={ proj.url }
-                            category={ proj.category }
-                            tolls={ proj.tolls }
-                            git={ proj.git }
-                        />
-                    ))
+                    !projects.length ?
+                        <span>Nenhum projeto encontrado</span> : projects
+                            .filter((item) => {
+                                if ( category === 'Todas' ) return Projects;
+                                return item.category === category;
+                            })
+                            .filter((proj) => 
+                                proj.name.toLocaleLowerCase().startsWith(searchName.toLocaleLowerCase())
+                            )
+                            .map((proj) => (
+                                <ProjectCard
+                                    key={ proj.name }
+                                    name={ proj.name }
+                                    img={ proj.img }
+                                    description={ proj.description }
+                                    url={ proj.url }
+                                    category={ proj.category }
+                                    tolls={ proj.tolls }
+                                    git={ proj.git }
+                                />
+                            ))
                 }
             </div>
         </div>
