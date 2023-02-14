d1a9141d369e54478329cdd314224e3961b46720
Istvan <olivaistvan@gmail.com>
2023-02-13 23:13:55 -0300

    refactor: (sendEmail) Change alert

--- portfolio/src/utils/emailsJs/sendEmail.js
+++ portfolio/src/utils/emailsJs/sendEmail.js
@@ -1,7 +1,8 @@
 import emailjs from '@emailjs/browser';
 
 const sendEmailjs = (serviceId, templateId, messages, userId) => {
-    emailjs.sendForm(serviceId, templateId, messages, userId);
+    emailjs.sendForm(serviceId, templateId, messages, userId)
+        .then(() => alert('Email enviado com sucesso !'));
 };
 
 export default sendEmailjs;
