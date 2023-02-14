f78e49b3c23f86a2a4a6fcec8d1e737dd86a045e
Istvan <olivaistvan@gmail.com>
2023-02-13 22:33:55 -0300

    refactor: (Header) Implement navbar

--- portfolio/src/Components/Header/Header.js
+++ portfolio/src/Components/Header/Header.js
@@ -1,12 +1,33 @@
 import { Link } from 'react-router-dom';
 import React from 'react';
+import '../Header/Header.css';
+
+import img from '../../images/icon-home.png';
 
 function Header() {
     return (
-        <header>
-            <Link to='/'>Início</Link>
-            <Link to='/projetos'>Projetos</Link>
-            <Link to='/contato'>Contato</Link>
+        <header className="header-container">
+            <div className="link-container">
+                <Link to='/'>
+                    <img src={ img } id="home-icon"/>
+                </Link>
+            </div>
+            <div className="header-nav">
+                <div className="link-container">
+                    <Link to='/projetos'>
+                        <button>
+                    Projetos
+                        </button>
+                    </Link>
+                </div>
+                <div className="link-container">
+                    <Link to='/contato'>
+                        <button>
+                    Contato
+                        </button>
+                    </Link>
+                </div>
+            </div>
         </header>
     );
 }
