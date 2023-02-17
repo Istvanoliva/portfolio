1433538dfc924250604dad0a9c186f71056a4939
Istvan <olivaistvan@gmail.com>
2023-02-17 15:38:42 -0300

    style: (Header) final touchs

--- portfolio/src/Components/Header/Header.js
+++ portfolio/src/Components/Header/Header.js
@@ -2,27 +2,29 @@ import { Link } from 'react-router-dom';
 import React from 'react';
 import '../Header/Header.css';
 
-import img from '../../images/icon-home.png';
+// import img from '../../images/icon-home.png';
 
 function Header() {
     return (
         <header className="header-container">
             <div className="link-container">
                 <Link to='/'>
-                    <img src={ img } id="home-icon"/>
+                    <button className="btn-header">
+                        Início
+                    </button>                    
                 </Link>
             </div>
             <div className="header-nav">
                 <div className="link-container">
                     <Link to='/projetos'>
-                        <button>
+                        <button className="btn-header">
                     Projetos
                         </button>
                     </Link>
                 </div>
                 <div className="link-container">
                     <Link to='/contato'>
-                        <button>
+                        <button className="btn-header">
                     Contato
                         </button>
                     </Link>
